17de2c33da4525441967b31791d544a43bd72f74
Rhapsodya Piedro Asmorobangun <piedrorhapsodya@gmail.com>
2025-09-10 15:42:51 +0700

    chore: 2.2.1 data

--- Comps/comp_rates_config.py
+++ Comps/comp_rates_config.py
@@ -42,10 +42,10 @@ with open(str(os.getenv("REPO_PATH")) + "/data/w-engine.json") as char_file:
     WENGINE = json.load(char_file)
 
 # no need to add 2.2.1"_da"
-RECENT_PHASE = "2.1.2"
+RECENT_PHASE = "2.2.1"
 
 # if no past phase, past_phase = "null"
-past_phase = "2.1.1"
+past_phase = "2.1.2"
 # if as: da_mode = True
 da_mode = args.deadly_assault
 
